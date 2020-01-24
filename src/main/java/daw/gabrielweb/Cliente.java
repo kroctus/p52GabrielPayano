d84e35bee805db73c0dbd91289568b5205c72cff
kroctus <gabriel21kun@gmail.com>
2020-01-24 13:38:38 +0100

    c7

---
+++ src/main/java/daw/gabrielweb/Cliente.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.gabrielweb;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Cliente {
+    
+    private String nombre;
+    private String email;
+    private int edad;
+
+    public Cliente() {
+        this.nombre="Gabriel";
+        this.email="gabriel21kun@gmail.com";
+        this.edad=19;
+    }
+    
+    
+    
+}
